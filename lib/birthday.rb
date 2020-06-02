f2654ac28b7cd4b5b22cd96cd780356c59308d55
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-02 20:49:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,4 +13,10 @@ end
 
 
 def age_appropriate_birthday(birthday_kids)
-  
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  if age > 20 
+    puts ""
+
+
+end 
